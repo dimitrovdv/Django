bd61e6e3a39cdc8abc3e91025b623e1546c36ef9
dimitrovdv <dimitrovdv@bk.ru>
2024-05-18 19:48:46 +0300

    commit m2m done

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,40 @@
 from django.contrib import admin
+from django.forms import BaseInlineFormSet
+from django.core.exceptions import ValidationError
+from .models import Article,Scope,Tag
 
-from .models import Article
 
+class ArticleFormSet(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            is_main = form.cleaned_data['is_main']
+
+            if is_main:
+                count += 1
+            if count>1:
+                raise ValidationError ('Основным может быть только один раздел')
+            if is_main == False:
+                raise ValidationError('Укажите основной раздел')
+
+        return super().clean()
+
+
+
+
+class ScopeAdmin(admin.TabularInline):
+    model = Scope
+    formset = ArticleFormSet
+    extra = 1
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ScopeAdmin]
+
+@admin.register(Tag)
+class Tag(admin.ModelAdmin):
+    model = Tag
+
+
+
+
--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -8,7 +8,7 @@ def students_list(request):
     template = 'school/students_list.html'
     context = {}
     ordering = 'group'
-    students = Student.objects.order_by(ordering).prefetch_related('teacher')
-    print(students)
+    students = Student.objects.order_by(ordering).prefetch_related('articles')
+    #print(students)
     context['object_list'] = students
     return render(request, template, context)
